1932f1a1bb16821b5ecf7c01e720e7e5211f97a4
shiba <shibadog0410@gmail.com>
2020-05-15 02:24:20 +0900

    init sample app

---
+++ resources/js/components/ExampleComponent.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="container">
+    <div class="row justify-content-center">
+      <div class="col-md-8">
+        <div class="card card-default">
+          <div class="card-header">Example Component</div>
+          <div class="card-body">I'm an example component.</div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.test {
+  color: red;
+}
+</style>
+
+<script>
+export default {
+  props: {
+    test: String
+  },
+
+  mounted() {
+    console.log("ExampleComponent mounted.");
+  }
+};
+</script>
